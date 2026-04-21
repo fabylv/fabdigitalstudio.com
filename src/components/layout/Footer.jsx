904d9cff776538caf49360ddbcdd72f84ac84e61
Faby <fabylv@gmail.com>
2026-04-21 12:06:05 -0400

    updated path to logo in footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,4 @@
-import logo from '/fabdigitalstudio-logo-transparent.png';
+import logo from '../../assets/fabdigitalstudio-logo.png';
 import Container from '../ui/Container';
 import { footerContent } from '../../data/siteContent';
 
